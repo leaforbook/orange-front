0411a841cd129e85c4e19861d417d80fab2f09e4
xiaoyilin <240688248@qq.com>
2018-12-08 18:52:40 +0800

    修改样式

--- src/components/main/header.js
+++ src/components/main/header.js
@@ -3,17 +3,14 @@ import { NavBarItem,Tab } from "react-weui";
 import 'weui';
 import 'react-weui/build/packages/react-weui.css';
 import {Button} from "../login";
+import "../../item.css"
 
 
 export default class Header extends React.Component {
 
     render() {
         return (
-            <div >
-                <Tab type="navbar">
-                    <NavBarItem label="微商代理助手"></NavBarItem>
-                </Tab>
-            </div>
+            <div className="fill_space"> </div>
         )
     }
 }
